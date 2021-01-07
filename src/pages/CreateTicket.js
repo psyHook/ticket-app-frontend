2f923f8e5239cbfcdc8bf92aad03d088474c44ba
juan galarce <j6alarce@gmail.com>
2021-01-07 14:51:00 -0300

    Creandi pantalla para crear un ticket

--- src/pages/CreateTicket.js
+++ src/pages/CreateTicket.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Button, Col, Row, Typography } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
+
+const { Title, Text } = Typography;
 
 export const CreateTicket = () => {
-    return (
-        <div>
-            Create Ticket Page
-        </div>
-    )
-}
+  const newTicket = () => {
+    console.log("Nuevo ticket");
+  };
+
+  return (
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Title level={3}>Press the button for a new ticket</Title>
+
+          <Button
+            type="primary"
+            shape="round"
+            icon={<DownloadOutlined />}
+            size="large"
+            onClick={newTicket}
+          >
+            New Ticket
+          </Button>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: 100 }}>
+        <Col span={14} offset={6} align="center">
+          <Text level={2}>Your number</Text>
+          <br />
+          <Text type="success" style={{ fontSize: 55 }}>
+            66
+          </Text>
+        </Col>
+      </Row>
+    </>
+  );
+};
