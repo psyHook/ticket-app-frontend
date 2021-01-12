ee1a3a18c80125d51fc0a96cb392c49ccfe99a0a
Juan Galarce <j6alarce@gmail.com>
2021-01-12 11:50:05 -0300

    Reciviendo el ticket del backend y mostrandolo en la interfaz

--- src/pages/CreateTicket.js
+++ src/pages/CreateTicket.js
@@ -1,47 +1,51 @@
-import React, { useContext } from 'react';
-import { Button, Col, Row, Typography } from 'antd';
-import { DownloadOutlined } from '@ant-design/icons';
-import { useHideMenu } from '../hooks/useHideMenu';
-import { SocketContext } from '../context/SocketContext';
+import React, { useContext, useState } from "react";
+import { Button, Col, Row, Typography } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
+import { useHideMenu } from "../hooks/useHideMenu";
+import { SocketContext } from "../context/SocketContext";
 
 const { Title, Text } = Typography;
 
 export const CreateTicket = () => {
-
   useHideMenu(true);
 
-  const { socket } = useContext( SocketContext );
+  const { socket } = useContext(SocketContext);
+  const [ticket, setTicket] = useState(null);
 
   const newTicket = () => {
-    socket.emit('request-ticket');
+    socket.emit("request-ticket", null, (ticket) => {
+      setTicket(ticket);
+    });
   };
 
   return (
     <>
       <Row>
-        <Col span={14} offset={6} align='center'>
+        <Col span={14} offset={6} align="center">
           <Title level={3}>Press the button for a new ticket</Title>
 
           <Button
-            type='primary'
-            shape='round'
+            type="primary"
+            shape="round"
             icon={<DownloadOutlined />}
-            size='large'
+            size="large"
             onClick={newTicket}
           >
             New Ticket
           </Button>
         </Col>
       </Row>
-      <Row style={{ marginTop: 100 }}>
-        <Col span={14} offset={6} align='center'>
-          <Text level={2}>Your number</Text>
-          <br />
-          <Text type='success' style={{ fontSize: 55 }}>
-            67
-          </Text>
-        </Col>
-      </Row>
+      {ticket && (
+        <Row style={{ marginTop: 100 }}>
+          <Col span={14} offset={6} align="center">
+            <Text level={2}>Your number</Text>
+            <br />
+            <Text type="success" style={{ fontSize: 55 }}>
+              {ticket.number}
+            </Text>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
