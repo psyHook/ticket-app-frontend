6c0715043b39c208c871bcf6122fb294640595da
Juan Galarce <j6alarce@gmail.com>
2021-01-12 11:32:30 -0300

    Conectando la app de react con el socket server

--- src/TicketApp.js
+++ src/TicketApp.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Socket } from "socket.io-client";
+import { SocketProvider } from "./context/SocketContext";
 import { UiProvider } from "./context/UiContext";
-import { RouterPages } from './pages/RouterPages';
+import { RouterPages } from "./pages/RouterPages";
 
 export const TicketApp = () => {
   return (
-    <UiProvider>
-      <RouterPages />
-    </UiProvider>
+    <SocketProvider>
+      <UiProvider>
+        <RouterPages />
+      </UiProvider>
+    </SocketProvider>
   );
 };
--- src/pages/CreateTicket.js
+++ src/pages/CreateTicket.js
@@ -1,30 +1,32 @@
-import React from "react";
-import { Button, Col, Row, Typography } from "antd";
-import { DownloadOutlined } from "@ant-design/icons";
-import { useHideMenu } from "../hooks/useHideMenu";
+import React, { useContext } from 'react';
+import { Button, Col, Row, Typography } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+import { useHideMenu } from '../hooks/useHideMenu';
+import { SocketContext } from '../context/SocketContext';
 
 const { Title, Text } = Typography;
 
 export const CreateTicket = () => {
 
+  useHideMenu(true);
 
-  useHideMenu( true );
+  const { socket } = useContext( SocketContext );
 
   const newTicket = () => {
-    console.log("Nuevo ticket");
+    socket.emit('request-ticket');
   };
 
   return (
     <>
       <Row>
-        <Col span={14} offset={6} align="center">
+        <Col span={14} offset={6} align='center'>
           <Title level={3}>Press the button for a new ticket</Title>
 
           <Button
-            type="primary"
-            shape="round"
+            type='primary'
+            shape='round'
             icon={<DownloadOutlined />}
-            size="large"
+            size='large'
             onClick={newTicket}
           >
             New Ticket
@@ -32,10 +34,10 @@ export const CreateTicket = () => {
         </Col>
       </Row>
       <Row style={{ marginTop: 100 }}>
-        <Col span={14} offset={6} align="center">
+        <Col span={14} offset={6} align='center'>
           <Text level={2}>Your number</Text>
           <br />
-          <Text type="success" style={{ fontSize: 55 }}>
+          <Text type='success' style={{ fontSize: 55 }}>
             67
           </Text>
         </Col>
