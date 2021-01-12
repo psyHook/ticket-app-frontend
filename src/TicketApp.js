065bd37b52ec007076f68578bc37bdc614ba89b6
Juan Galarce <j6alarce@gmail.com>
2021-01-12 12:38:00 -0300

    Asignando el ticket para trabajar

--- src/TicketApp.js
+++ src/TicketApp.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Socket } from "socket.io-client";
 import { SocketProvider } from "./context/SocketContext";
 import { UiProvider } from "./context/UiContext";
 import { RouterPages } from "./pages/RouterPages";
--- src/pages/Desktop.js
+++ src/pages/Desktop.js
@@ -1,28 +1,36 @@
-import React, { useState } from 'react';
-import { Button, Col, Divider, Row, Typography } from 'antd';
-import { CloseCircleOutlined, RightOutlined } from '@ant-design/icons';
-import { useHideMenu } from '../hooks/useHideMenu';
-import { getUserStorage } from '../helpers/getUserStorage';
-import { Redirect, useHistory } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { Button, Col, Divider, Row, Typography } from "antd";
+import { CloseCircleOutlined, RightOutlined } from "@ant-design/icons";
+import { useHideMenu } from "../hooks/useHideMenu";
+import { getUserStorage } from "../helpers/getUserStorage";
+import { Redirect, useHistory } from "react-router-dom";
+import { SocketContext } from "../context/SocketContext";
 
 const { Title, Text } = Typography;
 
 export const Desktop = () => {
   useHideMenu(false);
+
   const [user] = useState(getUserStorage());
+  const { socket } = useContext(SocketContext);
+  const [ticket, setTicket] = useState(null);
+
   const history = useHistory();
 
   const exit = () => {
     localStorage.clear();
-    history.replace('/join');
+    history.replace("/join");
   };
 
   const nextTicket = () => {
-    console.log('Siguiente Ticket');
+    console.log(user);
+    socket.emit("next-ticket-to-work", user, (ticket) => {
+      setTicket(ticket);
+    });
   };
 
   if (!user.agent || !user.desktop) {
-    return <Redirect to='/join' />;
+    return <Redirect to="/join" />;
   }
 
   return (
@@ -31,11 +39,11 @@ export const Desktop = () => {
         <Col span={20}>
           <Title level={2}>{user.agent}</Title>
           <Text>You are working at the desk: </Text>
-          <Text type='success'>{user.desktop}</Text>
+          <Text type="success">{user.desktop}</Text>
         </Col>
 
-        <Col span={4} align='right'>
-          <Button shape='round' type='danger' onClick={exit}>
+        <Col span={4} align="right">
+          <Button shape="round" type="danger" onClick={exit}>
             <CloseCircleOutlined />
             Exit
           </Button>
@@ -43,20 +51,21 @@ export const Desktop = () => {
       </Row>
 
       <Divider />
+      {ticket && (
+        <Row>
+          <Col>
+            <Text>You are attending ticket number: </Text>
+            <Text style={{ fontSize: 30 }} type="danger">
+              {" "}
+              {ticket.number}
+            </Text>
+          </Col>
+        </Row>
+      )}
 
       <Row>
-        <Col>
-          <Text>You are attending ticket number: </Text>
-          <Text style={{ fontSize: 30 }} type='danger'>
-            {' '}
-            55
-          </Text>
-        </Col>
-      </Row>
-
-      <Row>
-        <Col offset={18} span={6} align='right'>
-          <Button onClick={nextTicket} shape='round' type='primary'>
+        <Col offset={18} span={6} align="right">
+          <Button onClick={nextTicket} shape="round" type="primary">
             <RightOutlined />
             Next
           </Button>
